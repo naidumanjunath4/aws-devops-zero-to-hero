8772f6c465a6b11f6fa42beba936ad553ebdd151
naidumanjunath4 <40971505+naidumanjunath4@users.noreply.github.com>
2023-09-29 08:46:12 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -4,5 +4,12 @@ set -e
 # Pull the Docker image from Docker Hub
 docker pull naidumanjunath4/simple-python-flask-app
 
+#Stopping the running container
+sudo docker stop prototype
+
+#Removing the docker container
+sudo docker rm prototype
+
 # Run the Docker image as a container
-docker run -d -p 5005:5005 naidumanjunath4/simple-python-flask-app
+sudo docker run -d --name prototype naidumanjunath4/simple-python-flask-app
+#docker run -d -p 5005:5005 naidumanjunath4/simple-python-flask-app
